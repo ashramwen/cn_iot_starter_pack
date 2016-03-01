659e4eeb0f36fbaaa3fbcbf489c57d51566df0e1
Zhanwen xu <leon.xu@kii.com>
2016-03-01 14:42:59 +0800

    added app initialisation

--- extension_server/api/controllers/FirmwaresController.js
+++ extension_server/api/controllers/FirmwaresController.js
@@ -5,29 +5,107 @@ module.exports = {
 		var appID = req.headers['x-app-id']
 		var appKey = req.headers['x-app-key']
 		var site = req.headers['x-app-site']
-
 		var host = sails.config.kiiSite[site]
 
-		var request = require("request");
+		var initBucketACL = function(bucketID, callback){
+			var options = {
+				method: 'POST',
+				url: 'https://' + host + '/api/apps/' +appID+ '/buckets/' + bucketID + '/objects',
+				headers: {
+					'content-type': 'application/vnd.kii.ObjectCreationResponse+json',
+					authorization: authorization,
+					'x-kii-appkey': appKey,
+					'x-kii-appid': appID
+				},
+				body: '{}'
+			};	
 
-		var options = {
-			method: 'DELETE',
-			url: 'https://' + host + '/api/apps/' + appID + '/buckets/FIRMWARE_BUCKET/acl/CREATE_OBJECTS_IN_BUCKET/UserID:ANONYMOUS_USER',
-			headers: {
-				'x-kii-appid': appID,
-				'x-kii-appkey': appKey,
-				authorization: authorization
-			}
-		};
+			sails.request(options, function(error, response, body) {
+				if (error) {
+					res.badRequest(error)
+					return
+				}
+				if (response.statusCode != 201) {
+					res.badRequest(body)
+					return
+				}
+				var objectID = JSON.parse(body).objectID	
 
-		console.log(options)
+				var options = {
+					method: 'DELETE',
+					url: 'https://' + host + '/api/apps/' + appID + '/buckets/' + bucketID + '/objects/' + objectID,
+					headers: {
+						'content-type': 'application/json',
+						authorization: authorization,
+						'x-kii-appkey': appKey,
+						'x-kii-appid': appID
+					}
+				};	
 
-		request(options, function(error, response, body) {
-			if (error) throw new Error(error);
+				sails.request(options, function(error, response, body) {
+					if (error) {
+						res.badRequest(error)
+						return
+					}
+					if (response.statusCode != 204) {
+						res.badRequest(body)
+						return
+					}	
 
-			console.log(body);
-			console.log(response.statusCode)
-			res.ok(response.statusCode, body)
-		});
+					var options = {
+						method: 'DELETE',
+						url: 'https://' + host + '/api/apps/' + appID + '/buckets/' + bucketID + 
+							'/acl/CREATE_OBJECTS_IN_BUCKET/UserID:ANY_AUTHENTICATED_USER',
+						headers: {
+							'x-kii-appid': appID,
+							'x-kii-appkey': appKey,
+							authorization: authorization
+						}
+					};	
+
+					sails.request(options, function(error, response, body) {
+						if (error) {
+							res.badRequest(error)
+							return
+						}
+						if (response.statusCode != 204) {
+							res.badRequest(body)
+							return
+						}
+						var options = {
+							method: 'DELETE',
+							url: 'https://' + host + '/api/apps/' + appID + '/buckets/' + bucketID + 
+								'/acl/DROP_BUCKET_WITH_ALL_CONTENT/UserID:ANY_AUTHENTICATED_USER',
+							headers: {
+								'x-kii-appid': appID,
+								'x-kii-appkey': appKey,
+								authorization: authorization
+							}
+						}
+						sails.request(options, function(error, response, body) {
+							if (error) {
+								res.badRequest(error)
+								return
+							}
+							if (response.statusCode != 204) {
+								res.badRequest(body)
+								return
+							}	
+							callback()
+						})
+					});
+				})
+			});
+		}
+
+		initBucketACL("FIRMWARE_BUCKET", function(){
+			initBucketACL("MODEL_BUCKET", function() {
+				initBucketACL("TAG_BUCKET", function() {
+					initBucketACL("FIRMWARE_NAMESPACE", function () {
+						res.ok('app initialisation finished')
+					})
+				})
+			})
+		})
 	}
 }
