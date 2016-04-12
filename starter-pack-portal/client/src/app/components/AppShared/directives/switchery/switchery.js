555811db56bfcfb77d6eeccc15e7776824400747
augustine <ashramwen@gmail.com>
2016-04-12 14:23:34 +0800

    update state

--- starter-pack-portal/client/src/app/components/AppShared/directives/switchery/switchery.js
+++ starter-pack-portal/client/src/app/components/AppShared/directives/switchery/switchery.js
@@ -2,18 +2,25 @@ angular.module('StarterPack.AppShared').directive('switchery', ['$timeout', func
     return {
         restrict: 'E',
         scope: {
-            ngModel: '=?',
-            reverse: '=?'
+            on: '=ngModel',
+            reverse: '=?',
+            ngChange: '&'
         },
         templateUrl: 'app/components/AppShared/directives/switchery/switchery.template.html',
         replace: true,
         link: function(scope, element, attrs) {
-            scope.on = scope.ngModel;
+            // scope.on = scope.ngModel;
             scope.yesText = attrs['yesText'] || '';
             scope.noText = attrs['noText'] || '';
             scope.switch = function() {
                 if (attrs.hasOwnProperty('readonly')) return;
                 scope.on = !scope.on;
+            }
+            if (scope.ngChange && typeof(scope.ngChange) === 'function') {
+                scope.$watch('on', function(newValue, oldValue) {
+                    if (newValue === undefined || oldValue === undefined) return;
+                    scope.$eval(scope.ngChange);
+                });
             }
         }
     };
